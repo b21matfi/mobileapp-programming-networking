cae415710c2ade497badacf9b6003467d141aa0b
b21matfi <b21matfi@student.his.se>
2022-05-10 16:25:50 +0200

    Added a Button and recycler view to the app. Added so that i can get the data from the json file.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+    RecyclerView recyclerView;
+    private Button GetData;
+    private Button ClearData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetData = findViewById(R.id.Get_Data);
+        GetData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new JsonTask(MainActivity.this).execute(JSON_URL);
+                }
+            });
+
+        }
 
         new JsonFile(this, this).execute(JSON_FILE);
     }
